f43c4fb6e0b5ef3bdf63ff3ef570d69ce329d1cd
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2021-08-21 22:11:49 +0000

    Add Function to display name that the user entered before answering questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,44 @@
-let form = document.getElementById('name-form');
-form.addEventListener('submit', formCheck);
+document.addEventListener("DOMContentLoaded", function(){
+    let html = `
+        <h2>Please provide your name</h2>
+        <!--First Name-->
+        <form id="name-form">
+            <div>
+                <label for="name">Full Name:</label>
+                <input type="text" name="full_name" id="name" class="form-input">
+            </div>
+            <!--Button to submit your details-->
+            <div>
+                <button class="play-button" type="submit">Lets Play!</button>
+                <button class="play-button" type="reset">Reset</button>
+            </div>
+    `;
+    let gameBox = document.getElementById('game-box');
 
-function formCheck(event){
-    event.preventDefault();
-    let name = document.getElementById('name').value;
+    gameBox.innerHTML = html;
 
-    if(name === null || name === ""){
-        alert('Please provide a name and email address');
-        console.log("This is working");
-    }else{
-        form.submit();
-        welcomeName();
+    let form = document.getElementById('name-form');
+    form.addEventListener('submit', formCheck);
+
+    function formCheck(event){
+        event.preventDefault();
+        const name = form.elements['name'].value;
+
+        if(name === null || name === ""){
+            alert("Please enter your name");
+        }else{
+            playGame();
+        }
     }
-}
+})
 
-function welcomeName(){
-    let userName = document.getElementById('name');
+function playGame(){
+    let userName = document.getElementById('name').value;
     let welcomeBox = document.getElementById('game-box');
     let welcomeMessage = `
     <h2>Welcome to the quiz ${userName}</h2>
     `;
     
     welcomeBox.innerHTML = welcomeMessage;
+    console.log(userName);
 }
