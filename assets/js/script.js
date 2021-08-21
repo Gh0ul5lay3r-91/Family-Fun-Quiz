30950a53c3f3b81b961a12a5b833c10189c830b3
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2021-08-21 10:40:23 +0000

    Add fix for form validation, Removed email address as it is not required

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,23 @@ form.addEventListener('submit', formCheck);
 function formCheck(event){
     event.preventDefault();
     let name = document.getElementById('name').value;
-    let email = document.getElementById('eaddress').value;
 
     if(name === null || name === ""){
         alert('Please provide a name and email address');
         console.log("This is working");
     }else{
         form.submit();
+        welcomeName();
     }
 }
 
 function welcomeName(){
-    let userName = document.getElementById('name');
+    let userName = document.getElementById('form-input');
     let welcomeBox = document.getElementById('welcome-box');
     let welcomeMessage = `
     <h2>Welcome to the quiz ${userName}</h2>
     `;
-
+    
     welcomeBox.innerHTML = welcomeMessage;
 }
 
