1a9717fa888489f2bedcb63a2e407f5382725ed1
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2021-08-23 07:24:26 +0000

    Added structure changes to Quiz page and added styling

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -1,73 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const userName = localStorage.getItem('name');
+    let userScore = document.getElementById('user-score');
 
-    startGame();
+    userScore.innerHTML = `
+    <p class="results">${userName} your score is:<span id="correct">0</span></p>
+    `;
 })
-
-const quizContainer = document.getElementById('game-box');
-const questions = [
-    {
-        question: "Who invented JavaScript?",
-        answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which one of these is a JavaScript package manager?",
-        answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-        },
-        correctAnswer: "d"
-    }
-];
-
-function startGame(){
-  // variable to store the HTML output
-  const output = [];
-
-  // for each question...
-  questions.forEach(
-    (currentQuestion, questionNumber) => {
-
-      // variable to store the list of possible answers
-      const answers = [];
-
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
-
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-    }
-  );
-
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
-}
